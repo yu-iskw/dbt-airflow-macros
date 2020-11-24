3aecc7b1783aa598d0c3ff50dd95dd9b4c00b126
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-24 23:22:04 +0900

    Update

--- integration_tests/postgres/run_tests.sh
+++ integration_tests/postgres/run_tests.sh
@@ -8,18 +8,21 @@ profile="integration_tests"
 target="postgres"
 
 # Install dbt packages
+echo "Execute 'ebt deps'"
 dbt deps \
   --profiles-dir "$profiles_dir" \
   --profile "$profiles" \
   --target "$target"
 
 # Run dbt models
+echo "Execute 'ebt run'"
 dbt run \
   --profiles-dir "$profiles_dir" \
   --profile "$profiles" \
   --target "$target"
 
 # Run dbt tests
+echo "Execute 'ebt test'"
 dbt test \
   --profiles-dir "$profiles_dir" \
   --profile "$profiles" \
