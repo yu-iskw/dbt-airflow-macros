43e5e02320d65c86bc76bb3ec3a76f7c418f3db5
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-24 16:54:20 +0900

    Update

--- ci/install_python_modules.sh
+++ ci/install_python_modules.sh
@@ -6,4 +6,4 @@ set -e
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
-pip3 install --no-cache-dir --force-reinstall -r "${PROJECT_DIR}/dev/requirements-dev.txt"
+pip3 install --no-cache-dir --force-reinstall -r "${PROJECT_DIR}/dev/requirements-0.18.txt"
