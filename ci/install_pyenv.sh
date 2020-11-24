59ff34fbacb0cdac0ac432cb561a0560b1c73a89
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-24 17:08:00 +0900

    Update

--- ci/install_pyenv.sh
+++ ci/install_pyenv.sh
@@ -7,12 +7,12 @@ PYENV_ROOT="${HOME}/.pyenv"
 # Install pyenv
 git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
 
+# Install python 3.8 as default
+"${PYENV_ROOT}"/bin/pyenv install 3.8.6
+"${PYENV_ROOT}"/bin/pyenv global 3.8.6
+
 # Set environment variables
 # shellcheck disable=SC2016
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 # shellcheck disable=SC2016
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-
-# Install python 3.8 as default
-"${PYENV_ROOT}"/bin/pyenv install 3.8.6
-"${PYENV_ROOT}"/bin/pyenv global 3.8.6
