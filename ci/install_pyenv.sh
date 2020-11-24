1990794b3dbb3de6d8d036417e385e1f8d700b42
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-24 16:56:37 +0900

    Update

--- ci/install_pyenv.sh
+++ ci/install_pyenv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Constants
+PYENV_ROOT="${HOME}/.pyenv"
+
 # Install pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
 
 # Set environment variables
 # shellcheck disable=SC2016
@@ -11,4 +14,4 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 
 # Update pyenv database
-pyenv update
+"${PYENV_ROOT}"/pyenv update
