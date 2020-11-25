88d6438114edb0307fc5dd424f5098cc0f4aedad
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-25 09:17:11 +0900

    Update

--- integration_tests/postgres/models/test_macros.sql
+++ integration_tests/postgres/models/test_macros.sql
@@ -5,7 +5,7 @@
 }}
 
 SELECT
-  '{{ dbt_airflow_macros.ds() }}' AS ds,
-  '{{ dbt_airflow_macros.ds_nodash() }}' AS ds_nodash,
-  '{{ dbt_airflow_macros.ts() }}' AS ts,
-  '{{ dbt_airflow_macros.ts_nodash() }}' AS ts_nodash
+  E'{{ dbt_airflow_macros.ds() }}' AS ds,
+  E'{{ dbt_airflow_macros.ds_nodash() }}' AS ds_nodash,
+  E'{{ dbt_airflow_macros.ts() }}' AS ts,
+  E'{{ dbt_airflow_macros.ts_nodash() }}' AS ts_nodash
