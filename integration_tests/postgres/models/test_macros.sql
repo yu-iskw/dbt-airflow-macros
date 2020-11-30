4212895e91a2282e16df34c6ca5d81c68c46f9a2
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2020-11-30 09:30:48 +0900

    Fix

--- integration_tests/postgres/models/test_macros.sql
+++ integration_tests/postgres/models/test_macros.sql
@@ -7,5 +7,5 @@
 SELECT
   TO_DATE(E'{{ dbt_airflow_macros.ds() }}', 'YYYY-MM-DD') AS ds,
   TO_DATE(E'{{ dbt_airflow_macros.ds_nodash() }}', 'YYYYMMDD') AS ds_nodash,
-  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts() }}', 'YYYY-MM-DDT%HH:MI:SS+00:00') AS ts,
-  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts_nodash() }}', 'YYYYMMDDT%HHMISS') AS ts_nodash
+  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts() }}', 'YYYY-MM-DDT%HH24:MI:SS+00:00') AS ts,
+  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts_nodash() }}', 'YYYYMMDDT%HH24MISS') AS ts_nodash
