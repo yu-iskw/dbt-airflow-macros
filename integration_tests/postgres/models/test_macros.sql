5f5a42bcb7a5a8cf34f9d6dae630446d596f2352
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2022-03-02 18:39:13 +0900

    Fix

--- integration_tests/postgres/models/test_macros.sql
+++ integration_tests/postgres/models/test_macros.sql
@@ -12,4 +12,4 @@ SELECT
   TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts() }}', 'YYYY-MM-DDT%HH24:MI:SS+00:00') AS ts,
   TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts(timezone="Asia/Tokyo") }}', 'YYYY-MM-DDT%HH24:MI:SS+00:00') AS ts_with_timezone,
   TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts_nodash() }}', 'YYYYMMDDT%HH24MISS') AS ts_nodash,
-  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts_nodash(timezone="Asia/Tokyo") }}', 'YYYYMMDDT%HH24MISS') AS ts_nodash_with_timezone,
+  TO_TIMESTAMP(E'{{ dbt_airflow_macros.ts_nodash(timezone="Asia/Tokyo") }}', 'YYYYMMDDT%HH24MISS') AS ts_nodash_with_timezone
