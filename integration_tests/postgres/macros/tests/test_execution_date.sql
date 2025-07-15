a0f98568b305198163101940cbd2b41cc08ab70f
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-07-16 02:00:10 +0900

    update
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- integration_tests/postgres/macros/tests/test_execution_date.sql
+++ integration_tests/postgres/macros/tests/test_execution_date.sql
@@ -1,15 +1,15 @@
 {% macro test_execution_date() %}
 
   {# Test case 1: EXECUTION_DATE not set, no timezone #}
-  {% set expected = modules.datetime.datetime.utcnow().isoformat() %}
-  {% set actual = dbt_airflow_macros.execution_date() %}
-  {{ dbt_unittest.assert_equals(expected, actual, 'test_execution_date_utcnow_no_timezone') }}
+  {% set expected = modules.datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f') | reverse | cut(9) | reverse %}
+  {% set actual = dbt_unittest.get_result("{{ dbt_airflow_macros.execution_date() }}", {"EXECUTION_DATE": "none"}) %}
+  {{ dbt_unittest.assert_true(actual.startswith(expected), 'test_execution_date_utcnow_no_timezone - Expected: starts with ' ~ expected ~ ', Actual: ' ~ actual) }}
 
   {# Test case 2: EXECUTION_DATE not set, with timezone #}
   {% set expected_dt = modules.datetime.datetime.utcnow() %}
-  {% set expected = modules.pytz.timezone("Asia/Tokyo").localize(expected_dt).isoformat() %}
-  {% set actual = dbt_airflow_macros.execution_date(timezone="Asia/Tokyo") %}
-  {{ dbt_unittest.assert_equals(expected, actual, 'test_execution_date_utcnow_with_timezone') }}
+  {% set expected = modules.pytz.timezone("Asia/Tokyo").localize(expected_dt).strftime('%Y-%m-%dT%H:%M:%S.%f') | reverse | cut(9) | reverse %}
+  {% set actual = dbt_unittest.get_result("{{ dbt_airflow_macros.execution_date(timezone=\"Asia/Tokyo\") }}", {"EXECUTION_DATE": "none"}) %}
+  {{ dbt_unittest.assert_true(actual.startswith(expected), 'test_execution_date_utcnow_with_timezone - Expected: starts with ' ~ expected ~ ', Actual: ' ~ actual) }}
 
   {# Test case 3: EXECUTION_DATE set, no timezone in macro call #}
   {% set env_var_val = "2023-01-15T10:30:00" %}
@@ -20,7 +20,7 @@
   {# Test case 4: EXECUTION_DATE set, with timezone in macro call #}
   {% set env_var_val = "2023-01-15T10:30:00" %}
   {% set expected_dt = modules.datetime.datetime.fromisoformat(env_var_val) %}
-  {% set expected = modules.pytz.timezone("America/New_York").localize(expected_dt).isoformat() %}
+  {% set expected = modules.pytz.timezone("America/New_York").localize(expected_dt).isoformat(timespec='seconds') %}
   {% set actual = dbt_unittest.get_result("{{ dbt_airflow_macros.execution_date(timezone=\"America/New_York\") }}", {"EXECUTION_DATE": env_var_val}) %}
   {{ dbt_unittest.assert_equals(expected, actual, 'test_execution_date_env_var_with_timezone_macro') }}
 
@@ -33,7 +33,7 @@
   {# Test case 6: EXECUTION_DATE set with offset, with different timezone in macro call #}
   {% set env_var_val = "2023-01-15T10:30:00+09:00" %}
   {% set expected_dt = modules.datetime.datetime.fromisoformat(env_var_val) %}
-  {% set expected = modules.pytz.timezone("Europe/London").localize(expected_dt).isoformat() %}
+  {% set expected = modules.pytz.timezone("Europe/London").localize(expected_dt).isoformat(timespec='seconds') %}
   {% set actual = dbt_unittest.get_result("{{ dbt_airflow_macros.execution_date(timezone=\"Europe/London\") }}", {"EXECUTION_DATE": env_var_val}) %}
   {{ dbt_unittest.assert_equals(expected, actual, 'test_execution_date_env_var_offset_with_timezone_macro') }}
 
